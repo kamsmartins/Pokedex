60394b7cffee13292923c3e5a8b7113cfed23e4f
Kamylla <134303200+kamsmartins@users.noreply.github.com>
2023-05-24 17:38:37 -0300

    Update poke-api.js

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -24,7 +24,7 @@ pokeApi.getPokemonDetail = (pokemon) => {
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `http://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
